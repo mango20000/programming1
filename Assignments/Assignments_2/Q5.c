391ef5788da16de9865f70c0cf3253381a22984e
mango20000 <kobepham2000@gmail.com>
2025-09-30 23:47:24 -0500

    test

---
+++ Assignments/Assignments_2/Q5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int number;
+    printf("Please enter an integer: ");
+    scanf("%d", &number);
+
+    if(number % 2 == 0){
+        printf("The number is Even\n");
+    }
+    else{
+        printf("The number is Odd\n");
+    }
+
+    return 0;
+}
---
+++ Assignments/Assignments_2/Q6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int number;
+    printf("Please enter an integer: ");
+    scanf("%d", &number);
+
+    if(number > 0){
+        printf("The number is positive\n");
+    }
+    else if(number < 0){
+        printf("The number is negative\n");
+    }
+    else if(number == 0){
+        printf("The number is 0\n");
+    }
+
+    return 0;
+}
