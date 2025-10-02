8209da413812cb29bc462019354d4231027354ef
mango20000 <kobepham2000@gmail.com>
2025-10-01 22:36:07 -0500

    test

--- Assignments/Assignment_2/Q7.c
+++ Assignments/Assignment_2/Q7.c
@@ -9,15 +9,15 @@ int main(){
     scanf("%d %d %d", &number1, &number2, &number3);
     //printf("%d, %d, %d \n", number1, number2, number3);
 
-    if(number1 >= number2 && number3){
+    if(number1 >= number2 &&  number1 >= number3){
         printf("The highest number is: %d\n", number1);
     }
-    else if(number2 >= number1 && number3){
+    else if(number2 >= number1 && number2 >= number3){
         printf("The highest number is: %d\n", number2);
     }
-    else if(number3 >= number1 && number2){
+    else if(number3 >= number1 && number3 >= number2){
         printf("The highest number is: %d\n", number3);
     }
-
+ 
     return 0;
 }
