2a5cecdc3c654c3bbaafb11b92b19f964abc2453
mango20000 <kobepham2000@gmail.com>
2025-10-13 14:00:45 -0500

    test

--- Assignments/Assignment_3/Q5.c
+++ Assignments/Assignment_3/Q5.c
@@ -4,15 +4,19 @@
 int main(){
     int n;
     char star = '*';
+    char space = ' ';
     printf("Please input number of rows(n); ");
     scanf("%d", &n);
 
-    for(int i=0;i<n;i++){
-       for(int j=0;j<=i;j++){
-        printf("%c", star);
-       }
-       printf("\n");
-    }
+    for(int i=n;i>=0;--i){
+        printf("%c", space);
 
+        for(int j=0;j<=n;j++){
+            for(int k=0;k<=j;k++){
+                printf("%c", star);
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
